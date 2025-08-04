307b536f7d4d0ca753335b9b715b7af71c5b799c
otavio <suaresotavio@gmail.com>
2025-08-04 16:12:33 -0300

    feat: adicionar seção de FAQ com interatividade e estilo

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,13 @@ menu_toggle.addEventListener("click", () => {
   header_nav.style.display = header_nav.style.display === "flex" ? "none" : "flex";
   header_buttons.style.display = header_buttons.style.display === "flex" ? "none" : "flex";
 });
+
+const plus_icon = document.querySelectorAll('.plus-icon');
+const faq_answer = document.querySelectorAll('.faq-answer');
+
+plus_icon.forEach((icon, index) => {
+  icon.addEventListener('click', () => {
+    faq_answer[index].classList.toggle('active');
+    icon.classList.toggle('active');
+  });
+});
